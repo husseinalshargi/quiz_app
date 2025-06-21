fdb3b835966dbeeff9c568d4e2eb762fa9748332
Hussein Adel <x.sky.x@hotmail.com>
2025-06-21 05:55:02 +0300

    implement question screen with dynamic question display and answer buttons

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +14,28 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenstate extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Center(child: Text('QuestionScreen'));
+    final currentQuestion = questions[0];
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+            //...: [[1,2,3],4] -> [1,2,3,4]
+          ],
+        ),
+      ),
+    );
   }
 }
